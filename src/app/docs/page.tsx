a0af76c0f282a50fefa8098fe096dd62992e7335
junhong <junhong@nextsecurities.com>
2025-12-22 18:45:43 +0900

    fix

---
+++ src/app/docs/page.tsx
@@ -0,0 +1,11 @@
+export default async function DocsPage() {
+  return (
+    <div>
+      <div>문서 페이지입니다.</div>
+      <div>아이템이 없습니다</div>
+      <div>1개의 아이템</div>
+      <div>2개의 아이템</div>
+      <div>변수: test</div>
+    </div>
+  );
+}
